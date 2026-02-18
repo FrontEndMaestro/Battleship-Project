e404290836044e61cd01d21997453b73f4206054
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-02-18 14:27:54 +0500

    test:Add tests to check if ship has sunk

--- tests/ship.spec.js
+++ tests/ship.spec.js
@@ -1,7 +1,7 @@
 import shipClass from "../src/ship.js";
 
 describe("Ship Class", () => {
-  let ship = new shipClass();
+  let ship = new shipClass(3);
   test("Ship class exists", () => {
     expect(typeof shipClass).toBe("function");
   });
@@ -14,4 +14,13 @@ describe("Ship Class", () => {
     expect(ship.isSunk).toBeDefined();
   });
 
+  test("ship is hurt but not dead", () => {
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
+  });
+  test("ship is dead", () => {
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
 });
