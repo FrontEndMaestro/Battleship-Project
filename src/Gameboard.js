2ad3a563e7552fb03b9017d609c988f06f6b8a83
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-02-21 00:41:28 +0500

    feat:Add method checkCoordinateValidity
    Check if the coordinates are between 0-10 (excluding 10, including 0).
    The start and end coordinates should match the length of the ship for
    them to be considered correct.

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,7 +1,8 @@
-import ship from "./ship";
+import ship from "./ship.js";
 import throwError from "./Error.js";
 export default class Gameboard {
   ships = [];
+  missedShotsCoordinates = [];
 
   constructor() {
     this.board = Array.from({ length: 10 }, () => new Array(10).fill(0));
@@ -19,8 +20,40 @@ export default class Gameboard {
 
   placeShips(startPosition, endPosition, shipID) {
     if (shipID >= this.ships.length) throwError("Hello");
-    let index = this.ships.findIndex((element) => element.id == shipID);
-    this.ships[index].position.push(startPosition);
-    this.ships[index].position.push(endPosition);
+    if (this.checkCordinateValidity(startPosition, endPosition, shipID)) {
+      this.ships[shipID].position.push(startPosition);
+      this.ships[shipID].position.push(endPosition);
+      return true
+    }
+
+    return false
+  }
+
+  checkCordinateValidity(startPosition, endPosition, index) {
+    let shipLength = this.ships[index].ship.getLength();
+    if (
+      startPosition.every((value) => value < 10 && value >= 0) &&
+      endPosition.every((value) => value < 10 && value >= 0)
+    ) {
+      if (
+        startPosition[0] + endPosition[0] + 1 == shipLength ||
+        startPosition[1] + endPosition[1] + 1 == shipLength
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  attackHit(x, y) {
+    return this.ships.findIndex(
+      (element) => element.position[0] == x && element.position[1] == y,
+    );
+  }
+
+  receiveAttack(x, y) {
+    let index = this.attackHit(x, y);
+    if (index != -1) this.ships[index].ship.hit();
+    else this.missedShotsCoordinates.push({ x, y });
   }
 }
