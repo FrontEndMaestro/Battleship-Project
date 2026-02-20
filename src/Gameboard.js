6618dcda6dd311ba61cd448128f53c3669338132
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-02-20 17:39:35 +0500

    feat:Add an array ships to store ships

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,6 +1,20 @@
+import ship from "./ship";
 export default class Gameboard {
-  board = Array.from({ length: 10 }, () => new Array(10).fill(0));
+  ships = [];
+
   constructor() {
-    
+    this.board = Array.from({ length: 10 }, () => new Array(10).fill(0));
+    this.initializeShips();
+  }
+
+  initializeShips() {
+    let sizes = [5, 4, 3, 3, 2];
+    for (let i = 0; i < 5; i++) {
+      this.ships.push({ ship: new ship(sizes[i]), position: [] });
+    }
+  }
+
+  placeShips(x, y) {
+    this.ships[4].position.push("11", "21");
   }
 }
