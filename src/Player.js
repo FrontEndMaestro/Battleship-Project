576d9311e56d46f1701adf4cf81c1d5ab6325741
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-03-02 16:33:45 +0500

    feat: Add scoring system for players

--- src/Player.js
+++ src/Player.js
@@ -1,6 +1,15 @@
-export default class Player{
-    constructor(type,gameBoard){
-        this.type=type
-        this.gameBoard=gameBoard
-    }
+export default class Player {
+  constructor(type, gameBoard) {
+    this.type = type;
+    this.gameBoard = gameBoard;
+    this.score = 0;
+  }
+
+  addScore() {
+    this.score++;
+  }
+
+  getScore() {
+    return this.score;
+  }
 }
