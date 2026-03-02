3bdca9ad68335ebca471ca9c51bf8f02792bc190
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-03-02 21:36:08 +0500

    feat(ui): implement game initialization logic

--- src/index.js
+++ src/index.js
@@ -11,39 +11,39 @@ window.addEventListener("DOMContentLoaded", () => {
   startGame(player1, player2);
 });
 
-function setEventListener(player1, player2, validateTurn) {
-  let gridCell = document.querySelectorAll(".cell");
-  gridCell.forEach((cell) => {
-    cell.addEventListener("click", (e) => {
-      if (validateTurn(e.target.parentElement.id)) {
-        console.log(e.target.id);
-        let currentPlayer =
-          e.target.parentElement.id == player1.type ? player1 : player2;
-        registerHit(e.target.id, currentPlayer);
-      }
-    });
-  });
-}
-
-function registerHit(coordinates, player) {
-  //reverse to convert into x and y format
-  let coordinateArray=coordinates.split("-").reverse()
-  player.gameBoard.receiveAttack(coordinateArray[0],coordinateArray[1])
-  
-}
-
 function startGame(player1, player2) {
-  setEventListener(player1, player2, validateTurn,updateBoards);
+  setEventListener(player1, player2, validateTurn, registerHit);
   let turn = player1.type;
 
   function changeTurn(playerType) {
     playerType == player1.type ? (turn = player2.type) : (turn = player1.type);
   }
 
-  function updateBoards(){
-    document.querySelector('body').innerHTML=""
-    renderGameboard(player1)
-    renderGameboard(player2)
+  function updateBoards() {
+    document.querySelector("body").innerHTML = "";
+    renderGameboard(player1);
+    renderGameboard(player2);
+    setEventListener(player1, player2, validateTurn, registerHit);
+  }
+
+  function registerHit(coordinates, player) {
+    //reverse to convert into x and y format
+    let coordinateArray = coordinates.split("-").reverse();
+    player.gameBoard.receiveAttack(coordinateArray[0], coordinateArray[1]);
+    updateBoards();
+  }
+
+  function setEventListener(player1, player2, validateTurn, registerHit) {
+    let gridCell = document.querySelectorAll(".cell");
+    gridCell.forEach((cell) => {
+      cell.addEventListener("click", (e) => {
+        if (validateTurn(e.target.parentElement.id)) {
+          let currentPlayer =
+            e.target.parentElement.id == player1.type ? player1 : player2;
+          registerHit(e.target.id, currentPlayer);
+        }
+      });
+    });
   }
 
   function validateTurn(gridId) {
@@ -54,4 +54,6 @@ function startGame(player1, player2) {
     }
     return false;
   }
+
+  function displayTurn() {}
 }
