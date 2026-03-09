8129274e3adc9ca1bf35550a7e8adf91233cb42e
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-03-09 22:57:06 +0500

    feat: player can view board before starting game
    Before starting the game players can view their board and randomize
    ship positions again if they choose to.Previously, ships could only be placed once.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
 import "./styles.css";
-import newGame from "./newGame";
+import placeHumanShips from "./placeShips";
+import { placeComputerShip } from "./placeShips";
 import renderGameboard from "./renderGameboard";
 import gameBoard from "./Gameboard";
 import Player from "./Player";
-import { computerAttack } from "./Computer";
+import { computerAttack, generateHitPosition } from "./Computer";
 
 window.addEventListener("DOMContentLoaded", () => {
   document.querySelector("body").innerHTML = "";
-  let { player1, player2 } = newGame();
-  renderGameboard(player1);
-  renderGameboard(player2);
-  startGame(player1, player2);
+  humanShipPlacement();
+
 });
+
+function humanShipPlacement() {
+  let player1 = new Player("human", new gameBoard());
+  placeHumanShips(player1);
+
+  const body = document.querySelector("body");
+  const randomizeBtn = document.createElement("button");
+  const startGameBtn = document.createElement("button");
+  startGameBtn.textContent = "Start";
+
+  randomizeBtn.classList.add("random-btn");
+  startGameBtn.classList.add("start-btn");
+
+  randomizeBtn.innerHTML = `
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+    >
+      <g fill="#fc2b2a">
+        <path d="M17 5h2v2h-2zM5 17h2v2H5zm6-14h2v6h-2zM9 1h2v8H9zm0 8h2v2H9zm10 8H9v2h10zM5 7H3v10h2z" />
+        <path d="M13 15h-2v6h2zm2-2h-2v8h2zm0 8h-2v2h2zM5 5h10v2H5zm14 12h2V7h-2z" />
+      </g>
+    </svg>`;
+
+  startGameBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#fc2b2a" d="M23.505 1.5v5.001h-1v2h-1v1h-1v-1h-1v-1h-1.001v-1h-1v-1h-1v-1h-1v-1h1v-1h2V1.5zM11.502 13.503v2h-1v1h-1v1h-1v1.001h-1v1H5.5v-2h1v-1h1v-1h1v-1h1v-1.001z"/><path fill="#fc2b2a" d="M19.504 9.502v-1h-1v-1h-1V6.5h-1v-1h-1v-1h-1.001v1h-1v1h-1v1h-1v1h-1.001v1H4.501v1h-1v1.001h-1v1H1.5v2h4v-1h1.001v-1h1v-1h1v-1h1v1h-1v1h-1v2h1v-1h1v-1h3.002v3.001h-1v1h-1.001v1h2v-1h1v-1h1.001v1h-1v1h-1v1h-1v1h-1.001v4.002h2v-1h1v-1h1.001v-1h1v-6.002h1v-1h1v-1h1v-1h1.001v-1.001h1v-1zm-2-1v2h-1v1h-2.001v-1h-1v-2h1v-1h2v1z"/></svg>`;
+  body.appendChild(randomizeBtn);
+  body.appendChild(startGameBtn);
+  renderGameboard(player1);
+
+  randomizeBtn.addEventListener("click", () => {
+    const gridContainer = document.querySelector(".game-board");
+    gridContainer.remove();
+    player1.gameBoard.resetBoard();
+    placeHumanShips(player1);
+    renderGameboard(player1);
+  });
+
+  startGameBtn.addEventListener("click", () => {
+    let player2 = new Player("Computer", new gameBoard());
+    placeComputerShip(player2);
+    //renderGameboard(player1);
+    renderGameboard(player2);
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+      button.remove();
+    });
+    startGame(player1, player2);
+  });
+}
 
 function startGame(player1, player2) {
   setEventListener(validateTurn, registerHit);
