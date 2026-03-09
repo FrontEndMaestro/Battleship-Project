fa09ec0dde135cc91c33deecea813ffb8cc22ad3
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-03-09 14:46:48 +0500

    refactor: decouple random logic from attack logic
    generateHitPosition is now passed as a callback to computerAttack.This
    allows us to mock generateHitPosition to return predictable values when
    testing.

--- src/Computer.js
+++ src/Computer.js
@@ -1,18 +1,16 @@
-
-function computerAttack(playerBoard) {
+function computerAttack(playerBoard, generateHitPosition) {
   let x = 0;
   let y = 0;
   do {
     ({ x, y } = generateHitPosition());
   } while (playerBoard.board[y][x] == "X" || playerBoard.board[y][x] == "O");
-
   return [x, y];
 }
 
 function generateHitPosition() {
-  let x = Math.floor(Math.random() * 9);
-  let y = Math.floor(Math.random() * 9);
+  let x = Math.floor(Math.random() * 10);
+  let y = Math.floor(Math.random() * 10);
   return { x, y };
 }
 
-export { computerAttack};
+export { computerAttack, generateHitPosition };
