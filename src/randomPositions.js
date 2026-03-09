e5e0767e28c87ed346a523b8f98bd86925b48da3
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-03-09 21:35:56 +0500

    feat: add method to generate random coordinates
    Ships are now placed by generating random coordinates according to each
    ships length.Ensured ships dont overlap eachother.
    Previously ship positions where hardcoded which made the game predictable

---
+++ src/randomPositions.js
@@ -0,0 +1,21 @@
+export default function generateRandomPositons(gameboard, shipID) {
+  let start = [];
+  let final = [];
+  let length = gameboard.ships[shipID].ship.getLength();
+  do {
+    start[0] = Math.floor(Math.random() * 10);
+    start[1] = Math.floor(Math.random() * 10);
+    let verticalOrHorizontal = Math.floor(Math.random() * 2);
+    if (verticalOrHorizontal == 1) {
+      //vertical
+      final[0] = start[0] + (length - 1);
+      final[1] = start[1];
+    } else {
+      //horizontal
+      final[0] = start[0];
+      final[1] = start[1] + (length - 1);
+    }
+  } while (!gameboard.checkCoordinateValidity(start, final, shipID));
+
+  return [start, final];
+}
