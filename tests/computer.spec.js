46cf5299ba3b9b50feb6e9139159e25a5396e64d
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-03-09 14:52:11 +0500

    test: verify attack logic with mocked coordinates

--- tests/computer.spec.js
+++ tests/computer.spec.js
@@ -1,7 +1,6 @@
+import { generateHitPosition } from "../src/Computer";
 import gameBoard from "../src/Gameboard";
-import { computerTurn } from "../src/Computer";
-
-
+import { computerAttack } from "../src/Computer";
 
 jest.mock("../src/Computer", () => {
   const originalModule = jest.requireActual("../src/Computer");
@@ -11,24 +10,29 @@ jest.mock("../src/Computer", () => {
     ...originalModule,
     generateHitPosition: jest
       .fn()
-      .mockReturnValueOnce({x:0,y:0})
-      .mockReturnValueOnce({x:1,y:0})
-      .mockReturnValueOnce({x:2,y:0})
+      .mockReturnValueOnce({ x: 0, y: 0 })
+      .mockReturnValueOnce({ x: 1, y: 0 })
+      .mockReturnValueOnce({ x: 2, y: 0 }),
   };
 });
-
-
-it("generates coordinates from 0-9", () => {
-  let obj=generateHitPosition()
-  expect(obj.x).toBeGreaterThan(-1);
-  expect(obj.x).toBeLessThanOrEqual(9);
-});
-
 
 it("does not shoot the same coordinate twice", () => {
   let gameboard = new gameBoard();
   gameboard.board[0][0] = "X";
   gameboard.board[0][1] = "O";
-  
-  expect(computerTurn(gameboard)).toEqual([2,0])
+
+  expect(computerAttack(gameboard, generateHitPosition)).toEqual([2, 0]);
+});
+
+it("generates coordinates from 0-9", () => {
+  jest.resetModules();
+  jest.unmock("../src/Computer");
+  let computerModule = require("../src/Computer");
+  let gameboard = require("../src/Gameboard").default;
+  let arr = computerModule.computerAttack(
+    new gameboard(),
+    computerModule.generateHitPosition,
+  );
+  expect(arr[0]).toBeGreaterThan(-1);
+  expect(arr[0]).toBeLessThanOrEqual(9);
 });
